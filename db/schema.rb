c5eb884758af4351d1e7f354e704a17199bb7b73
Katsuki <Katsuki_Sato0130@icloud.com>
2018-12-22 13:14:25 +0900

    Update gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717065241) do
-
-  create_table "constant_expenses", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "category"
-    t.integer "amount"
-    t.date "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180607075339) do
 
   create_table "expenses", force: :cascade do |t|
     t.string "title"
